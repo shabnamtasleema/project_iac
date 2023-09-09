46c7e12798740028debe93426d18718bb7c5fce7
Shabnam <tasleema2210@gmail.com>
2023-09-09 13:40:43 +0530

    feat: tic-006 | Fixed the code to avoid recreation of resources - Resolved the interdependent resources - Implemented count as a conditional resource creation - Used state commands to manage the statefile resource list - state rm mv list - Imported few resources using terraform import - As an engineer, we should make sure terraform shouldnot recreate some   of the critical resources in production. So we take care of this using   terraform state commands and import commands when we implement the   conditional resource creation.

--- sg.tf
+++ sg.tf
@@ -14,8 +14,6 @@ resource "aws_security_group" "instance_sc2" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
-
-
   tags = {
     Name = "${var.environment}-${var.security_grp}-1"
   }
@@ -53,22 +51,7 @@ resource "aws_security_group" "instance_sc1" {
   name        = "${var.environment}-${var.security_grp}-2"
   description = "Allow TLS inbound traffic"
   vpc_id      = var.aws_vpc_id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "ssh"
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
+  
   tags = {
     Name = "${var.environment}-${var.security_grp}-2"
   }
