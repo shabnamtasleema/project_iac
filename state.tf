23390231c85a0a6032ee4d9bc93dade02659666b
Shabnam <tasleema2210@gmail.com>
2023-09-11 16:34:55 +0530

    feat: tic-008 | testing terraform commands in jenkinsfile

--- state.tf
+++ state.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket  = "my-s3-bucket-new-shab"
-    key     = var.key
-    region  = "us-west-1"
-    profile = "shabnam"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket  = "my-s3-bucket-new-shab"
+#     key     = var.key
+#     region  = "us-west-1"
+#     profile = "shabnam"
+#   }
+# }
